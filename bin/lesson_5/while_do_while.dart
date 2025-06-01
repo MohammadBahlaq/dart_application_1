6db492b426d492ab6cd14e04ae0eba42575259a3
Mohammad Bahlaq <Bahlaq57@gmail.com>
2025-06-01 13:45:39 -0700

    while and do-while

---
+++ bin/lesson_5/while_do_while.dart
@@ -0,0 +1,14 @@
+void main() {
+  List list = [1, 2, 3, 4, 5, 6];
+
+  // while (list.isNotEmpty) {
+  //   print(list);
+  //   list.removeLast();
+  // }
+
+  List list2 = [];
+
+  do {
+    print(list2);
+  } while (list2.isNotEmpty);
+}
